38e686c57e02f86d0bc02a903cd4fd59bbc86320
jdpatilonline <jdpatilonline@gmail.com>
2025-12-10 02:15:27 -0800

    Update owasp-dependency-check_old.sh

--- owasp-dependency-check_old.sh
+++ owasp-dependency-check_old.sh
@@ -1,29 +1,20 @@
 #!/bin/sh
-echo "Creating Temp Directory"
+
 OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directories"
     mkdir -p "$DATA_DIRECTORY"
     chmod -R 777 "$DATA_DIRECTORY"
+
     mkdir -p "$REPORT_DIRECTORY"
     chmod -R 777 "$REPORT_DIRECTORY"
 fi
-# Missing Purge Step
-echo "Checking for and running database purge to prevent corruption..."
-
-docker run --rm \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
-    owasp/dependency-check \
-    --purge
-
-echo "Downloading the Image"
 
 # Make sure we are using the latest version
 docker pull owasp/dependency-check
-
-echo "vulnerability Scanning"
 
 docker run --rm \
     --volume "`pwd`":/src \
